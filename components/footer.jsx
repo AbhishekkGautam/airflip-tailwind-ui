3d809602390232e31e2f663a4cc2d2b5675eaa34
Abhishek Gautam <abhishekgautam@Abhisheks-MacBook-Pro.local>
2023-12-26 23:59:37 +0530

    initial commit

---
+++ components/footer.jsx
@@ -0,0 +1,37 @@
+const Footer = () => {
+  return (
+    <section className="bg-gray-50">
+      <div className="container mx-auto py-8 flex justify-between items-center">
+        <div className="flex space-x-6">
+          <div className="text-sm">
+            <h1 className="text-gray-900 font-medium pb-1">Free Shipping </h1>
+            <p className="text-gray-500">
+              Get 2-day free shipping anywhere in North America.
+            </p>
+          </div>
+          <div className="text-sm">
+            <h1 className="text-gray-900 font-medium pb-1">Free Shipping </h1>
+            <p className="text-gray-500">
+              Get 2-day free shipping anywhere in North America.
+            </p>
+          </div>
+        </div>
+        <div className="flex space-x-6 items-center">
+          <div className="text-right">
+            <h1 className="font-bold text-3xl text-gray-900">$2,717</h1>
+            <p className="text-sm text-gray-500 pt-1">
+              Need financing? <span className="underline">Learn more</span>{' '}
+            </p>
+          </div>
+          <div className="">
+            <button className="bg-gray-900 px-6 py-3 rounded-md text-white text-sm font-medium">
+              Buy now
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
---
+++ components/image-container.jsx
@@ -0,0 +1,48 @@
+import Image from 'next/image';
+
+const ImageContainer = () => {
+  return (
+    <>
+      <div className="border border-gray-200 rounded-md overflow-hidden">
+        <Image
+          src="/img/kemper-front.jpg"
+          width={500}
+          height={340}
+          alt=""
+          className=""
+        />
+      </div>
+      <div className="flex mt-3 space-x-3 overflow-hidden">
+        <div className="border border-gray-900 rounded-md overflow-hidden">
+          <Image
+            src="/img/kemper-front.jpg"
+            width={500}
+            height={340}
+            alt=""
+            className=""
+          />
+        </div>
+        <div className="border border-gray-200 rounded-md overflow-hidden">
+          <Image
+            src="/img/kemper-angle.jpg"
+            width={500}
+            height={340}
+            alt=""
+            className=""
+          />
+        </div>
+        <div className="border border-gray-200 rounded-md overflow-hidden">
+          <Image
+            src="/img/kemper-rear.jpg"
+            width={500}
+            height={340}
+            alt=""
+            className=""
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ImageContainer;
